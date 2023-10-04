a1551a86eef133219f73eb36d6a1ab853b2e3ac9
Monstaar2805 <l201080238@iztapalapa.tecnm.mx>
2023-10-03 19:59:05 -0600

    Inicia Programacion De La Interfaz

---
+++ Interfaz.py
@@ -0,0 +1,15 @@
+from tkinter import*
+
+raiz=Tk()
+
+raiz.title("Maquina CNC 2D")
+
+raiz.resizable(True, True)
+
+raiz.iconbitmap("Icono.ico")
+
+raiz.geometry("1200x600")
+
+raiz.config(bg = "Purple")
+
+raiz.mainloop()
